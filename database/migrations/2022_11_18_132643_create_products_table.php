762fa575ca02567b84b10d50b4aa41bd46e87ddd
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-18 17:34:29 +0400

    .

---
+++ database/migrations/2022_11_18_132643_create_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_eng');
+            $table->text('description');
+            $table->text('description_eng');
+            $table->integer('price_rub');
+            $table->integer('price_usd');
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
