6c038191bf182abec6f9689fd16eb1a5aa6c9b3e
Mikhail Abramishvili <mg@abramishvili.net>
2022-11-25 16:05:20 +0400

    .

--- resources/js/components/front/Cart.vue
+++ resources/js/components/front/Cart.vue
@@ -1,40 +1,65 @@
 <template>
-    <div class="row">
-        <div class="col-12 col-lg-8">
-            <div v-for="cartItem in cart" :key="'cartItem_' + cartItem.sku" class="cart-item">
-                <div class="row align-items-center">
-                    <div class="col cart-item-col-name">
-                        <a :href="'/product/' + cartItem.id">
-                            {{ cartItem.name }}
-                        </a>
-                    </div>
-                    <div class="col cart-item-col-quantity">
-                        <button @click="updateQuantityMinus(cartItem)" class="btn btn-sm">-</button>
-                        <input @change="updateQuantity(cartItem)" type="number" :id="'quantity_' + cartItem.sku" :value="cartItem.quantity" min="1" class="form-control w-25 d-inline-flex cart-item-amount">
-                        <button @click="updateQuantityPlus(cartItem)" class="btn btn-sm">+</button>
-                    </div>
-                    <div class="col cart-item-col-price">
-                        <strong v-if="lang == 'en'">
-                            ${{ cartItem.price_usd * cartItem.quantity }}
-                        </strong>
-                        
-                        <strong v-else>
-                            {{ cartItem.price_rub * cartItem.quantity }} ₽
-                        </strong>
-                    </div>
-                    <div class="col cart-item-col-del">
-                        <button @click="remove(cartItem.sku)" class="btn btn-sm btn-outline-danger">&times;</button>
+    <div v-if="cart && lang" class="cart-wrapper">
+        <div v-if="views.loading" class="card-loader">
+            <div class="spinner-border text-dark" role="status">
+                <span class="visually-hidden">Загрузка...</span>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-lg-12">
+                <div v-for="cartItem in cart" :key="'cartItem_' + cartItem.sku" class="cart-item">
+                    <div class="row align-items-center">
+                        <div class="col cart-item-col-name">
+                            <a :href="'/product/' + cartItem.id">
+                                <template v-if="lang == 'en'">{{ cartItem.name_eng }}</template>
+                                <template v-else>{{ cartItem.name }}</template>
+                            </a>
+                        </div>
+                        <div class="col cart-item-col-quantity">
+                            <button @click="updateQuantityMinus(cartItem)" class="btn btn-sm btn-outline-standard">-</button>
+                            <input @change="updateQuantity(cartItem)" type="number" :id="'quantity_' + cartItem.sku" :value="cartItem.quantity" min="1" class="form-control w-25 d-inline-flex cart-item-amount">
+                            <button @click="updateQuantityPlus(cartItem)" class="btn btn-sm btn-outline-standard">+</button>
+                        </div>
+                        <div class="col cart-item-col-price">
+                            <strong v-if="lang == 'en'">
+                                ${{ cartItem.price_usd * cartItem.quantity }}
+                            </strong>
+                            
+                            <strong v-else>
+                                {{ cartItem.price_rub * cartItem.quantity }} ₽
+                            </strong>
+                        </div>
+                        <div class="col cart-item-col-del">
+                            <button @click="remove(cartItem.sku)" class="btn btn-sm btn-outline-danger">&times;</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-lg-4">
-            <div class="cart-panel">
-                <h5>В корзине</h5>
-                <!-- <p>{{ cart_products_total_quantity }} {{ cart_products_total_quantity | dgt_products }}<template v-if="cart_addons_total_quantity && cart_addons_total_quantity > 0">, {{ cart_addons_total_quantity }} {{ cart_addons_total_quantity | dgt_addons }}</template></p>
-                <h4>{{ cart_total_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") }} ₽</h4> -->
-
-                <button class="btn btn-standard">Перейти к оформлению</button>
+            <div class="col-12 cart-total">
+                <p v-if="lang == 'en'">Total products: <strong>{{ totalQuantity }}</strong> and total price is <strong>${{ totalPrice }}</strong></p>
+                <p v-else>Всего товаров: <strong>{{ totalQuantity }}</strong> на сумму <strong>{{ totalPrice }} руб.</strong></p>
+            </div>
+            <div class="col-12 col-lg-12">
+                <div class="cart-panel">
+                    <div class="row align-items-end">
+                        <div class="col-12 col-lg-9">
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <label class="form-label">Имя</label>
+                                    <input v-model="name" type="text" class="form-control">
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <label class="form-label">Телефон</label>
+                                    <input v-model="tel" type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <button class="btn btn-standard w-100">Оформить заказ</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -48,6 +73,9 @@
         data() {
             return {
                 cart: '',
+
+                name: '',
+                tel: '',
                 
                 views: {
                     loading: true,
@@ -56,14 +84,38 @@
         },
         mounted() {
             this.getCartInfo()
+
+            this.views.loading = false
+        },
+        computed: {
+            totalQuantity() {
+                if(this.cart) {
+                    return this.cart.map(c => c.quantity).reduce((acc, curr) => { return acc + curr }, 0)
+                }
+            },
+            totalPrice() {
+                if(this.cart) {
+                    let price = []
+
+                    if(this.lang == 'en') {
+                        this.cart.forEach(cartItem => {
+                            price.push(cartItem.quantity * cartItem.price_usd)
+                        })
+                    } else {
+                        this.cart.forEach(cartItem => {
+                            price.push(cartItem.quantity * cartItem.price_rub)
+                        })
+                    }
+
+                    return price.reduce((acc, curr) => { return acc + curr }, 0)
+                }
+            },
         },
         methods: {
             getCartInfo() {
                 axios.get('/_cart_data')
                 .then(response => {
                     this.cart = response.data
-
-                    this.views.loading = false
                 })
             },
             updateCart() {
